02aaf80b6f8976b696334118123340c158b7f731
Bruceja <2103932jagessar@zuyd.nl>
2024-04-02 19:18:46 +0200

    parameterwaarde aanpassingen

--- hyperparameters.py
+++ hyperparameters.py
@@ -1,5 +1,3 @@
-# https://www.educative.io/answers/implement-neural-network-for-classification-using-scikit-learn
-
 import pandas as pd
 import numpy as np
 import random
@@ -10,12 +8,7 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report 
 from sklearn.preprocessing import StandardScaler
 
-# dataset = load_digits()
-dataset = pd.read_csv('UNSW_NB15.csv',sep=',').sample(20000)
-# print(dataset.head())
-# print(dataset.info())
-# print(dataset.isnull().sum())
-# print(dataset['label'].value_counts())
+dataset = pd.read_csv('UNSW_NB15.csv',sep=',').sample(2000)
 
 # https://pythonprogramming.net/working-with-non-numerical-data-machine-learning-tutorial/
 def handle_non_numerical_data(dataset):
@@ -51,12 +44,8 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-#print(X_train[:10])
-
 def get_accuracy_score(individual):
     global X_train, y_train, X_test, y_test
-    # convertedValues = convert_values_to_string(individual)
-    # mlpc = MLPClassifier(hidden_layer_sizes=[50,50,50], max_iter=50, alpha=0.001, activation='identity', learning_rate_init=0.1,)
     mlpc = MLPClassifier(**individual)
     mlpc.fit(X_train, y_train)
     pred_mlpc = mlpc.predict(X_test)
@@ -65,7 +54,7 @@ def get_accuracy_score(individual):
     print(confusion_matrix(y_test, pred_mlpc))
 
     cm = accuracy_score(y_test, pred_mlpc)
-    #print(cm)
+    print(cm)
     return cm
     
 def fitness(individual):
@@ -74,19 +63,19 @@ def fitness(individual):
     if score == 1:
         return 99999
     else:
-        return abs(1/score)
+        return abs(score)
 
 def mutate(individual):
     # Kans op mutatie voor 'hidden_layer_sizes' 
     r = random.randint(1, 50)
     if r == 1:
-        individual['hidden_layer_sizes'] = (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10))
+        individual['hidden_layer_sizes'] = (random.randint(1, 5), random.randint(1, 5), random.randint(1, 5))
         print(f"Dit individu is gemuteerd en heeft een nieuwe waarde voor hidden layers: {individual['hidden_layer_sizes']}")
     
     # Kans op mutatie voor 'max_iter'
     r = random.randint(1, 50)
     if r == 1:
-        individual['max_iter'] = random.randint(1, 10)
+        individual['max_iter'] = random.randint(1, 5)
         print(f"Dit individu is gemuteerd en heeft een nieuwe waarde voor max iter: {individual['max_iter']}")
 
     # Kans op mutatie voor 'activation'
@@ -112,6 +101,7 @@ def mutate(individual):
 # Initiële populatie genereren. Elke waarde is willekeurig
 # De omvang van de getallen kan aangepast worden. (Dus (1, 50) kan omgezet worden naar (1, 100), bijvoorbeeld)
 # Let op: een grote range kan er voor zorgen dat het algoritme erg lang duurt.
+# Wanneer de ranges hier veranderd worden, moet dat ook in de mutatiefunctie (hierboven) gedaan worden!
 population = []
 activation_functions = ['relu', 'tanh', 'logistic', 'identity']
 best_individuals = []
@@ -120,8 +110,8 @@ print("De initiële populatie wordt nu gegenereerd...")
 for i in range(100):
     print(f"Individu {i} wordt gegenereerd.")
     population.append(
-        {'hidden_layer_sizes': (random.randint(1, 10), random.randint(1, 10), random.randint(1, 10)),
-         'max_iter': random.randint(1, 10),
+        {'hidden_layer_sizes': (random.randint(1, 5), random.randint(1, 5), random.randint(1, 5)),
+         'max_iter': random.randint(1, 5),
          'activation': random.choice(activation_functions),
          'alpha': round(random.uniform(0.001, 0.1), 3),
          'learning_rate_init': round(random.uniform(0.001, 0.1), 3)})
@@ -171,9 +161,6 @@ for j in range(10):
         newPopulation.append(newIndividual)
         print(f"Het individu {newIndividual} is toegevoegd aan de nieuwe generatie.")
     
-    #print(parameters)
-    #print(values)
-    #print(newPopulation[0])
     population = newPopulation
 
 accuracies = []
@@ -183,24 +170,3 @@ for g in range((len(best_individuals))):
     
 for h in range((len(best_individuals))):
     print(f"Het beste individu in generatie {h} is: {best_individuals[h][1]} met een accuracy van {accuracies[h]}.")
-
-
-
-
-""" print(population[0])
-print(population[0].keys())
-print(population[0].values()) """
-
-# DEBUGGING
-""" 
-
-mlpc = MLPClassifier(hidden_layer_sizes=(50,50,50), max_iter=50, alpha=0.001, activation='identity', learning_rate_init=0.1, )
-mlpc.fit(X_train, y_train)
-pred_mlpc = mlpc.predict(X_test)
-
-print(classification_report(y_test, pred_mlpc))
-print(confusion_matrix(y_test, pred_mlpc))
-
-cm = accuracy_score(y_test, pred_mlpc)
-print(cm)
-"""
